e4825c00e575a796f874e6b5e56963f236781d26
Jorge Thomas (Akrista) <23145794+Akrista@users.noreply.github.com>
2026-05-29 18:16:17 +0000

    feat: added gh cli installation on debian

--- install.ps1
+++ install.ps1
@@ -263,6 +263,21 @@ if [ "$OS" = "Debian/Ubuntu" ]; then
     else
         echo "Pacstall is already installed."
     fi
+
+    if ! command -v gh &> /dev/null; then
+        echo "Installing GitHub CLI..."
+        sudo mkdir -p -m 755 /etc/apt/keyrings
+        out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg
+        cat "$out" | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null
+        sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg
+        sudo mkdir -p -m 755 /etc/apt/sources.list.d
+        echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+        sudo apt update
+        sudo apt install gh -y
+        rm -f "$out"
+    else
+        echo "GitHub CLI (gh) is already installed."
+    fi
 fi
 
 
